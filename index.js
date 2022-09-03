42fdb097452d310ca3c8c0abc13f53ee8b252635
victoriacabranes <victoriacabranesph@gmail.com>
2022-09-03 19:29:56 -0300

    Se corrige y agregan objetos + arrays

--- index.js
+++ index.js
@@ -1,26 +1,57 @@
+let opcion;
+let materias = [
+    {
+        materia: "Arte",
+        primerNota: 6,
+        segundaNota: 4,
+        tercerNota: 3,
+        promedio: 4
+    }
+];
 
-const nombreUsuario = prompt("Ingresa tu nombre");
-let materia = prompt("Hola " + nombreUsuario +  "!" + " Ingresa el nombre de la materia");
-let nota1 = Number(prompt("Ingresa tu primer nota"));
-let nota2 = Number(prompt("Ingresa tu segunda nota"));
-let nota3 = Number(prompt("Ingresa tu tercera nota"));
+while (opcion !== 0) {
+    opcion = Number(prompt("Elija una opción:\n1. Averiguar el promedio de una materia \n2. Ver mis promedios\n0. Salir" ));
 
-function calcularPromedio () {
-    let promedio = (nota1 + nota2 + nota3) / 3;
-    alert(nombreUsuario + " tu promedio en " + materia + " es de: " + promedio); 
+    switch (opcion) {
+        case 1:
+                const materia = prompt("Ingrese el nombre de la materia");
+                const primerNota = Number(prompt("Ingrese la primera nota"));
+                const segundaNota = Number(prompt("Ingrese la segunda nota"));
+                const tercerNota = Number(prompt("Ingrese la tercer nota"));
+                const promedio = (primerNota + segundaNota + tercerNota) / 3;
+                alert("tu promedio es de: " + promedio); 
+                guardarMateria (materia, primerNota, segundaNota, tercerNota, promedio);
+            break;
+        case 2:
+                mostrarMateria();
+            break;
+        case 0:
+                alert("Gracias por su visita.")
+            break;
+        default:
+                alert("La opción ingresada no es correcta, intente nuevamente");
+            break;
+    }
 }
-calcularPromedio()
-var inputValue = Number(prompt("Queres saber el promedio de otra materia? Ingresa: 1 (si) o 2 (no)"));
 
-while (inputValue == 1) {
-    materia = prompt("Ingresa el nombre de la materia");
-    nota1 = Number(prompt("Ingresa tu primer nota"));
-    nota2 = Number(prompt("Ingresa tu segunda nota"));
-    nota3 = Number(prompt("Ingresa tu tercera nota"));
-    calcularPromedio();
-    inputValue = Number(prompt("Queres saber el promedio de otra materia? Ingresa: 1 (si) o 2 (no)"));
+function guardarMateria (materia, primerNota, segundaNota, tercerNota, promedio) {
+    materias.push({
+        materia,
+        primerNota,
+        segundaNota,
+        tercerNota,
+        promedio
+    });
 }
- 
+
+function mostrarMateria () {
+    console.log(materias)
+}
+
+
+
+
+
     
 
 
